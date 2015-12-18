657e65fe2cb936ebcc7720f6c51c3105092bdf44
Ausin Formica <austin.formica@gmail.com>
2015-12-18 16:05:10 -0500

    parseLocalJSON

--- apiFactory/ViewController.swift
+++ apiFactory/ViewController.swift
@@ -14,7 +14,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        parseLocalJSON()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    func parseLocalJSON(){
+        
+        let path : String = NSBundle.mainBundle().pathForResource("sampleJSON", ofType: "json") as String!
+        let jsonData = NSData(contentsOfFile: path) as NSData!
+        let readableJSON = JSON(data: jsonData, options: NSJSONReadingOptions.MutableContainers, error: nil)
+    
+        var users = readableJSON["Users"]
+        NSLog("\(users)")
+
     }
 
     override func didReceiveMemoryWarning() {
