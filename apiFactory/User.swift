2e6631cc82353fb0cd9789dd496b553a3d553694
Ausin Formica <austin.formica@gmail.com>
2015-12-20 19:03:24 -0500

    user struct

---
+++ apiFactory/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  apiFactory
+//
+//  Created by Austin Formica on 12/20/15.
+//  Copyright © 2015 citylims. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let firstName: String
+    let lastName: String
+    let phoneNumber: Int
+    let picture: String
+    let email: String
+    let rating: Int
+}
