f99c2da10b486254c5c4f200405bc853f13d39b9
Ausin Formica <austin.formica@gmail.com>
2015-12-20 19:06:36 -0500

    location struct

---
+++ apiFactory/Location.swift
@@ -0,0 +1,17 @@
+//
+//  Location.swift
+//  apiFactory
+//
+//  Created by Austin Formica on 12/20/15.
+//  Copyright © 2015 citylims. All rights reserved.
+//
+
+import Foundation
+
+struct Location {
+    let latitude: Double
+    let longitude: Double
+    let userId: Int
+    let createTs: Int
+    var available: Bool
+}
